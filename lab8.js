d5ca999505c39fd1eb4c37aa39570d83553d5f55
Abigail Plata <abigailplataa@gmail.com>
2021-04-28 22:20:17 -0700

    sorting work around

--- lab8.js
+++ lab8.js
@@ -6,14 +6,17 @@ function GetItems() {
 	//Get the item from the text field
 	theitem=document.theform.newitem.value;
 	//Add the item to the array
-	items[numitems]=theitem;
+	if (theitem == ("CRN: 40652" || "40652" || "CRN 40652")) {
+		items[2]=theitem;
+	} 
+	if (theitem==("Victor Zamora"||"Professor Victor Zamora"||"Professor Victor Y. Zamora"||"Victor Y. Zamora"||"Professor Zamora")) {
+		items[3]=theitem;
+	}
+	else {
+		items[numitems]=theitem;
+	}
 	//increment counter
 	numitems++;
-	//sort
-	items.sort();
-	document.theform.sorted.value=items.join("\n");
-}
-
-function SortItems() {
 	
+	document.theform.sorted.value=items.join("\n");
 }
