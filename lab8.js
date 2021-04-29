e86be7a740881123c18cecd3266a7f442ca592c2
Abigail Plata <abigailplataa@gmail.com>
2021-04-28 22:32:46 -0700

    Update lab8.js

--- lab8.js
+++ lab8.js
@@ -1,22 +1,22 @@
 //Initializing counter and array
-var numitems=0;
+var counter=0;
 var items = new Array();
 
 function GetItems() {
 	//Get the item from the text field
 	theitem=document.theform.newitem.value;
 	//Add the item to the array
-	if (theitem == ("CRN: 40652" || "40652" || "CRN 40652")) {
+	if (theitem == ("CRN: 40652" || "40652")) {
 		items[2]=theitem;
 	} 
 	if (theitem==("Victor Zamora"||"Professor Victor Zamora"||"Professor Victor Y. Zamora"||"Victor Y. Zamora"||"Professor Zamora")) {
 		items[3]=theitem;
 	}
 	else {
-		items[numitems]=theitem;
+		items[counter]=theitem;
 	}
 	//increment counter
-	numitems++;
+	counter++;
 	
 	document.theform.sorted.value=items.join("\n");
 }
