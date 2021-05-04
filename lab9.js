ca269186cf69c86458c5a1bc9d039da8ce3a220d
Abigail Plata <abigailplataa@gmail.com>
2021-05-03 21:31:23 -0700

    timegreet

---
+++ lab9.js
@@ -0,0 +1,23 @@
+now = new Date();
+
+hour_of_day = now.getHours();
+minute_of_hour = now.getMinutes();
+seconds_of_minute = now.getSeconds();
+
+document.write("<h2>");
+document.write(hour_of_day + ":" + minute_of_hour + ":" + seconds_of_minute);
+document.write("</h2");
+
+document.write("<p>");
+if  (hour_of_day < 10)  {
+     document.write("Good morning."); 
+}  else if ((hour_of_day >= 14) && (hour_of_day <= 17)) {
+     document.write("Good afternoon.");
+}  else if (hour_of_day >= 17)  { 
+     document.write("Good evening.");
+}  else  {
+     document.write("Good day.");
+}
+document.write("</p>");
+
+
