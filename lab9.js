c6454ae9b3a1ceeba7c1c1bdb0d8d0a6d180d6f1
Abigail Plata <abigailplataa@gmail.com>
2021-05-03 21:50:44 -0700

    working

--- lab9.js
+++ lab9.js
@@ -3,6 +3,7 @@ now = new Date();
 hour_of_day = now.getHours();
 minute_of_hour = now.getMinutes();
 seconds_of_minute = now.getSeconds();
+hour_of_day_ny = now.getHours() - 21;
 
 document.write("<h2>");
 document.write("It is "+hour_of_day + ":" + minute_of_hour + ":" + seconds_of_minute+".");
@@ -17,6 +18,23 @@ if  (hour_of_day < 10)  {
      document.write(" Good evening, LA!");
 }  else  {
      document.write(" Good day, LA!");
+}
+document.write("</p>");
+
+//New York time:
+document.write("<h2>");
+document.write("It is "+hour_of_day_ny + ":" + minute_of_hour + ":" + seconds_of_minute+".");
+document.write("</h2");
+
+document.write("<p>");
+if  (hour_of_day_ny < 10)  {
+     document.write(" Good morning, NY!"); 
+}  else if ((hour_of_day_ny >= 14) && (hour_of_day_ny <= 17)) {
+     document.write(" Good afternoon, NY!");
+}  else if (hour_of_day_ny >= 17)  { 
+     document.write(" Good evening, NY!");
+}  else  {
+     document.write(" Good day, NY!");
 }
 document.write("</p>");
 
